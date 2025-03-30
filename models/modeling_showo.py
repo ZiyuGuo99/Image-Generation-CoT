be1d3208186a0a94f165dc9dddf450c83dfbdb3e
Ziyu Guo <61613867+ZiyuGuo99@users.noreply.github.com>
2025-03-30 22:49:46 +0800

    Update modeling_showo.py

--- models/modeling_showo.py
+++ models/modeling_showo.py
@@ -19,6 +19,7 @@ from transformers import AutoConfig
 from .modeling_utils import ConfigMixin, ModelMixin, register_to_config
 from .sampling import cosine_schedule, mask_by_random_topk
 from .phi import PhiForCausalLM
+from typing import Optional
 
 class Showo(ModelMixin, ConfigMixin):
     _supports_gradient_checkpointing = True
@@ -69,6 +70,7 @@ class Showo(ModelMixin, ConfigMixin):
             max_seq_length=128,
             labels_mask_text=None,
             labels_mask_image=None,
+            dpo_forward: Optional[bool] = False,
             **kwargs,
     ):
 
@@ -76,7 +78,8 @@ class Showo(ModelMixin, ConfigMixin):
             logits = self.showo(input_ids=input_ids, attention_mask=attention_mask)['logits']
         else:
             logits = self.showo(inputs_embeds=input_embeddings, attention_mask=attention_mask)['logits']
-
+        if dpo_forward:
+            return logits, labels
         if labels is not None:
             # 1. Mask token prediction (discrete diffusion) for image generation
             # Note that, max_seq_length indicates the maximum number of text tokens, maybe a bit confused.
@@ -238,3 +241,99 @@ class Showo(ModelMixin, ConfigMixin):
                 break
 
         return result
+
+
+    def t2i_generate_dpo(
+            self,
+            input_ids: torch.LongTensor = None,
+            uncond_input_ids: torch.LongTensor = None,
+            attention_mask=None,
+            temperature=1.0,
+            timesteps=18,  # ideal number of steps is 18 in maskgit paper
+            guidance_scale=0,
+            noise_schedule=cosine_schedule,
+            generator: torch.Generator = None,
+            config=None,
+            **kwargs,
+    ):
+        """
+        Generate 1:1 similar to the original MaskGit repo
+        https://github.com/google-research/maskgit/blob/main/maskgit/libml/parallel_decode.py#L79
+        """
+        # begin with all image token ids masked
+        mask_token_id = self.config.mask_token_id
+        num_vq_tokens = config.model.showo.num_vq_tokens
+        num_new_special_tokens = config.model.showo.num_new_special_tokens
+
+        input_ids_minus_lm_vocab_size = input_ids[:, -(num_vq_tokens + 1):-1].clone()
+        input_ids_minus_lm_vocab_size = torch.where(input_ids_minus_lm_vocab_size == mask_token_id,
+                                                    mask_token_id,
+                                                    input_ids_minus_lm_vocab_size - config.model.showo.llm_vocab_size - num_new_special_tokens)
+
+        # for classifier-free guidance
+        if uncond_input_ids is not None:
+            uncond_prefix = uncond_input_ids[:, :config.dataset.preprocessing.max_seq_length + 1]
+        total_selected_probs = []
+        total_masking = []
+        total_sampled_ids = []
+        total_input_ids = []
+        for step in range(timesteps):
+            if uncond_input_ids is not None and guidance_scale > 0:
+                uncond_input_ids = torch.cat(
+                    [uncond_prefix, input_ids[:, config.dataset.preprocessing.max_seq_length + 1:]], dim=1)
+                model_input = torch.cat([input_ids, uncond_input_ids])
+                cond_logits, uncond_logits = self(model_input, attention_mask=attention_mask).chunk(2)
+                logits = (1 + guidance_scale) * cond_logits - guidance_scale * uncond_logits
+                logits = logits[:, -(num_vq_tokens + 1):-1,
+                         config.model.showo.llm_vocab_size + num_new_special_tokens:-1]
+            else:
+                logits = self(input_ids, attention_mask=attention_mask)
+                logits = logits[:, -(num_vq_tokens + 1):-1,
+                         config.model.showo.llm_vocab_size + num_new_special_tokens:-1]
+
+            probs = logits.softmax(dim=-1)
+            sampled = probs.reshape(-1, logits.size(-1))
+            # 256,8192
+            sampled_ids = torch.multinomial(sampled, 1, generator=generator)[:, 0].view(*logits.shape[:-1])
+
+            unknown_map = input_ids_minus_lm_vocab_size == mask_token_id
+            sampled_ids = torch.where(unknown_map, sampled_ids, input_ids_minus_lm_vocab_size)
+            # Defines the mask ratio for the next round. The number to mask out is
+            # determined by mask_ratio * unknown_number_in_the_beginning.
+            ratio = 1.0 * (step + 1) / timesteps
+            mask_ratio = noise_schedule(torch.tensor(ratio))
+            # Computes the probabilities of each selected tokens.
+            selected_probs = torch.gather(probs, -1, sampled_ids.long()[..., None])
+            selected_probs = selected_probs.squeeze(-1)
+
+            # Ignores the tokens given in the input by overwriting their confidence.
+            selected_probs = torch.where(unknown_map, selected_probs, torch.finfo(selected_probs.dtype).max)
+
+            # Gets mask lens for each sample in the batch according to the mask ratio.
+            mask_len = (num_vq_tokens * mask_ratio).floor().unsqueeze(0).to(logits.device)
+            # Keeps at least one of prediction in this round and also masks out at least
+            # one and for the next iteration
+            mask_len = torch.max(
+                torch.tensor([1], device=logits.device),
+                torch.min(unknown_map.sum(dim=-1, keepdim=True) - 1, mask_len)
+            )
+            # Adds noise for randomness
+            temperature = temperature * (1.0 - ratio)
+            masking = mask_by_random_topk(mask_len, selected_probs, temperature, generator=generator)
+
+            # Masks tokens with lower confidence.
+            input_ids[:, -(num_vq_tokens + 1):-1] = torch.where(masking, mask_token_id,
+                                                                sampled_ids + config.model.showo.llm_vocab_size
+                                                                + num_new_special_tokens)
+            input_ids_minus_lm_vocab_size = torch.where(masking, mask_token_id, sampled_ids)
+            if timesteps == 1:
+                total_input_ids.append(input_ids.unsqueeze(1))
+            if step != timesteps - 1:
+                total_masking.append(torch.bitwise_xor(unknown_map, masking).unsqueeze(1))
+                total_input_ids.append(input_ids.unsqueeze(1).clone())
+            else:
+                total_masking.append(torch.bitwise_xor(unknown_map, torch.zeros_like(unknown_map)).unsqueeze(1))
+            total_selected_probs.append(selected_probs.unsqueeze(1))
+            total_sampled_ids.append(sampled_ids.unsqueeze(1))
+
+        return sampled_ids, torch.cat(total_sampled_ids, dim=1), torch.cat(total_input_ids, dim=1)
